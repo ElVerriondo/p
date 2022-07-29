dea606ba4f206aceff47f80a4f9c3b4e613681f9
Sebas <sebaslg96@gmail.com>
2022-07-28 22:03:11 -0500

    agregando carpeta de clase 11-14

---
+++ curso-integracion-php/13-14/server.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "<pre>";
+var_dump($_FILES);
+echo "</pre>";
+
+$basename = $_FILES["imagen"]["name"];
+
+$image = $_FILES["imagen"]["tmp_name"];
+
+$ruta_a_subir = "img/$basename";
+
+move_uploaded_file($image, $ruta_a_subir);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="<?= $ruta_a_subir ?>" alt="">
+</body>
+</html>
