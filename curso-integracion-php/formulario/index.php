bfb8460c2434e62b939af8fbb753a1cbaa5c998c
Sebas <sebaslg96@gmail.com>
2022-08-03 08:05:18 -0500

    terminado formulario para la clase: 20

---
+++ curso-integracion-php/formulario/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de contacto</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="main">
+
+        <div class="form-container">
+            <form action="#">
+                <h1>Contactanos!</h1>
+
+                <div class="input-group">
+                    <label for="name">Nombre:</label>
+                    <input type="text" name="name" id="name">
+                </div>
+
+                <div class="input-group">
+                    <label for="email">email:</label>
+                    <input type="email" name="email" id="email">
+                </div>
+
+                <div class="input-group">
+                    <label for="subject">Asunto:</label>
+                    <input type="text" name="subject" id="subject">
+                </div>
+
+                <div class="input-group">
+                    <label for="message">Mensaje:</label>
+                    <textarea name="message" id="message"></textarea>
+                </div>
+
+                <button type="submit" class="btn">Enviar</button>
+
+            </form>
+        </div>
+        
+    </div>
+</body>
+</html>
